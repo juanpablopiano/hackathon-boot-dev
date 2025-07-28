fa810b36c5998b3e02cb650206af6c082caac171
Juan Gonzalez <jgu1@hotmail.com>
2025-07-28 01:36:30 -0400

    fix: fix deno.json

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -1,19 +1,28 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import path from "node:path";
+import path from 'node:path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-	kit: {
-		adapter: adapter(),
-		alias: {
-			"@types": path.resolve("../types/dnd/index.ts")
-		}
-
-	}
+  // Consult https://svelte.dev/docs/kit/integrations
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html',
+      precompress: false,
+      strict: false,
+    }),
+    alias: {
+      '@types': path.resolve('../types/dnd/index.ts'),
+    },
+    prerender: {
+      handleHttpError: 'warn',
+      entries: ['*'],
+    },
+  },
 };
 
 export default config;
